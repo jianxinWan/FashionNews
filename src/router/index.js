b46c4066ea86563ffe86cd0f1a79d0af6773ae5b
wanjianxin <1158810459@qq.com>
2019-06-10 15:45:17 +0800

    fix: run the code

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home/index.vue'
-import Login from '@/components/login/login.vue'
-import Mydoc from '@/components/Mydoc/index.vue'
-import SignUp from '@/components/Login/signUp.vue'
-import Detail from '@/components/News/detail.vue'
+import Home from '@/Home/index.vue'
+import Login from '@/login/login.vue'
+import Mydoc from '@/Mydoc/index.vue'
+import SignUp from '@/Login/signUp.vue'
+import Detail from '@/News/detail.vue'
 
 Vue.use(Router)
+
 export default new Router({
   routes: [{
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/mydoc',
-    name: 'mydoc',
-    component: Mydoc
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/signUp',
-    name: 'signUp',
-    component: SignUp
-  },
-  {
-    path: '/signUp',
-    name: 'signUp',
-    component: SignUp
-  },
-  {
-    path: '/detail/:id',
-    name: 'detail',
-    component: Detail
-  }
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/mydoc',
+      name: 'mydoc',
+      component: Mydoc
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/signUp',
+      name: 'signUp',
+      component: SignUp
+    },
+    {
+      path: '/signUp',
+      name: 'signUp',
+      component: SignUp
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: Detail
+    }
   ]
 })
